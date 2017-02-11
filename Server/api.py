6cd6ba3b03ce4dff83405255d349de0348ccd6c8
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2017-02-11 15:38:42 +0000

    build server side

---
+++ Server/api.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_restful import Resource, Api, abort
+import os
+
+from weather_fetch import get_weather
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Weather(Resource):
+    def get(self, city):
+        try:
+            weather = get_weather(city)
+            return {
+                "temperature": weather.get_temp(),
+                "description": weather.get_description(),
+                "place": weather.get_place(),
+                "icon": weather.get_icon()
+            }
+        except ConnectionError:
+            abort(503, "not able to get weather for %s" % city)
+
+api.add_resource(Weather, '/weather/<string:city>')
+
+if __name__ == '__main__':
+    ENV = None
+    try:
+        ENV = os.environ['Environment']
+    except KeyError:
+        pass
+
+    debug = False
+    if ENV == "DEV":
+        debug = True
+
+    app.run(debug=debug)
---
+++ Server/weather_fetch.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+with open("appid", "r") as f:
+    APP_ID = f.read().strip()
+
+
+class WeatherRes:
+    def __init__(self, res):
+        self.res = json.loads(res)
+
+    def get_temp(self):
+        return round(self.res["main"]["temp"])
+
+    def get_icon(self):
+        icon_map = {
+            '01d': '\uf00d',
+            '02d': '\uf002',
+            '03d': '\uf041',
+            '04d': '\uf013',
+            '09d': '\uf019',
+            '10d': '\uf008',
+            '11d': '\uf016',
+            '13d': '\uf064',
+            '50d': '\uf014',
+            '01n': '\uf077',
+            '02n': '\uf086',
+            '03n': '\uf041',
+            '04n': '\uf031',
+            '09n': '\uf028',
+            '10n': '\uf028',
+            '11n': '\uf016',
+            '13n': '\uf016',
+            '50n': '\uf014'
+          };
+
+        icon = self.res["weather"][0]["icon"]
+        return icon_map[icon]
+
+    def get_description(self):
+        return self.res["weather"][0]["description"]
+
+    def get_place(self):
+        city = self.res["name"]
+        country = self.res["sys"]["country"]
+        return "%s, %s" % (city, country)
+
+def get_weather(city):
+    q = {"q":city,"units":"metric","appid":APP_ID}
+    res = requests.get(URL, params=q)
+    if res.status_code != 200:
+        raise ConnectionError("Not able to get weather data from openweathermap.org")
+
+    return WeatherRes(res.text)
